a36dcf4f6bb3987fcd78322928d97f94e89f17c8
aymerick116 <aymericknoah97@gmail.com>
2024-05-02 19:53:51 -0400

    created first succesfulll post request for signup controller

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,19 @@ import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 4000
 
-app.get("/", (req,res) =>{
-    res.send("Hello World!!!")
-})
+dotenv.config();
 
-app.use("/api/auth", authRoutes)
+app.use(express.json()); 
+app.use("/api/auth", authRoutes);
+
+
+// app.get("/", (req,res) =>{
+//     res.send("Hello World!!!")
+// })
+
+
 
 
 app.listen(PORT, ()=>{
