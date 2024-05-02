7c1276ad70456440bc0b936ed7922a17a1dc945b
aymerick116 <aymericknoah97@gmail.com>
2024-05-02 15:02:37 -0400

    first commit, with project set and auth routes set up

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js"
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 4000
+
+app.get("/", (req,res) =>{
+    res.send("Hello World!!!")
+})
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`))
+
