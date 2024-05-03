db5939031c0e2cfa89bd8a4e2d2242ffce25dadc
aymerick116 <aymericknoah97@gmail.com>
2024-05-03 14:12:23 -0400

    finished message controller, user is able to send messages and get messages

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import cookieParser from "cookie-parser";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,7 +13,10 @@ const PORT = process.env.PORT || 4000
 dotenv.config();
 
 app.use(express.json()); 
+app.use(cookieParser())
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 // app.get("/", (req,res) =>{
